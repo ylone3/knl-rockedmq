d2b882200e29bbc7ead353bb747b0028e82fed88
ylone3 <46116903+ylone3@users.noreply.github.com>
2018-12-25 11:26:26 +0800

    Initial commit

---
+++ src/main/java/com/yl/rocketmq/controller/InitController.java
@@ -0,0 +1,14 @@
+package com.yl.rocketmq.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class InitController {
+
+	@GetMapping("/sayHello")
+    public String sayHello(){
+
+        return "hello";
+    }
+}
